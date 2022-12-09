7ed3c6d0920fde7f5313312dcf14738252bd8a43
shasank27 <shasankperiwal2027@gmail.com>
2022-12-09 16:29:42 +0530

    Trying it out

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, Link, Routes, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import User from "./user";
+import NotFound from "./notfound";
+import reportWebVitals from "./reportWebVitals";
+const routing = (
+  <Router>
+    {/* Code inside DIV will be available in all pages */}
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+      </ul>
+    </div>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/user" element={<User />} />
+      <Route path="*" element={<NotFound />} />
+      {/* if anyone types any wrong route, will be directed to the above one */}
+    </Routes>
+  </Router>
 );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(routing);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ORIGINAL CODE BELOW
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
